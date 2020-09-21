c7184d3a220e00fa567fcbc0705fb24f35b6ec36
rauldelary <rauldelary@gmail.com>
2020-09-21 11:00:31 -0300

    added post and get route for gender class and changes in build.grade

--- src/main/kotlin/com/carinhocasual/Server.kt
+++ src/main/kotlin/com/carinhocasual/Server.kt
@@ -8,10 +8,19 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import com.carinhocasual.app.App
+import com.carinhocasual.gender.Gender
+import io.ktor.request.*
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import java.util.*
+
+val app = App ()
 
 fun main() {
   embeddedServer(Netty, 8080) {
     routing {
+
       install(ContentNegotiation) {
         gson {
           setPrettyPrinting()
@@ -19,9 +28,28 @@ fun main() {
       }
 
       get("/") {
+        call.response.status(HttpStatusCode.OK)
         call.respondText("<h1>Hello Kotlin!</h1>", ContentType.Text.Html)
       }
 
+      post ("/gender") {
+        //create new gender object with data coming from request
+        //it is only consuming the field "label" from the request
+        val newGender = call.receive <Gender> ()
+
+        //the field uid is being setting here, based on the label
+        newGender.uid = UUID.nameUUIDFromBytes(newGender.label.toLowerCase().toByteArray()).toString ()
+
+        //store the new gender obj in app.gender
+        app.genders.add (newGender)
+        call.response.status(HttpStatusCode.Created)
+        call.respond (newGender)
+      }
+
+      get ("/genders") {
+        call.response.status(HttpStatusCode.OK)
+        call.respond (app.genders)
+      }
     }
   }.start(wait = true)
 }
---
+++ src/main/kotlin/com/carinhocasual/gender/Gender.kt
@@ -0,0 +1,8 @@
+package com.carinhocasual.gender
+import java.util.UUID
+
+class Gender (genderLabel: String) {
+    var uid: String? = null
+    val label: String = genderLabel
+}
+
