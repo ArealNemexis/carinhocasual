9f4528195bf1c6daa08bb72165ff2ec7eba08efe
rauldelary <rauldelary@gmail.com>
2020-09-21 23:03:50 -0300

    fixing somethings

--- src/main/kotlin/com/carinhocasual/Server.kt
+++ src/main/kotlin/com/carinhocasual/Server.kt
@@ -10,9 +10,11 @@ import io.ktor.server.netty.*
 import com.carinhocasual.sexual_orientation.SexualOrientation
 //import com.carinhocasual.interests.Interest
 import com.carinhocasual.app.App
+import io.ktor.request.*
+import io.ktor.response.*
 import java.util.*
 
-val call: Any
+//val call: Any
 val app = App ()
 
 fun main() {
@@ -25,81 +27,79 @@ fun main() {
         }
 
         get("/") {
-          io.ktor.application.call.response.status(HttpStatusCode.OK)
-          io.ktor.application.call.respondText("<h1>Hello Kotlin!</h1>", ContentType.Text.Html)
+          call.response.status(HttpStatusCode.OK)
+          call.respondText("<h1>Hello Kotlin!</h1>", ContentType.Text.Html)
         }
 
-              post("/sexual_orientation") {
+        post("/sexual_orientation") {
 
-                val newSexualOrientation = io.ktor.application.call.receive <SexualOrientation>()
+          val newSexualOrientation = call.receive <SexualOrientation>()
 
-                newSexualOrientation.id = UUID.nameUUIDFromBytes(newSexualOrientation.label.toLowerCase().toByteArray()).toString ()
+          newSexualOrientation.id = UUID.nameUUIDFromBytes(newSexualOrientation.label.toLowerCase().toByteArray()).toString ()
 
-                val alreadyExists: Boolean = app.sexualOrientation.any {it.id == newSexualOrientation.id}
+          val alreadyExists: Boolean = app.sexualOrientation.any {it.id == newSexualOrientation.id}
 
-                if (alreadyExists) {
-                  io.ktor.application.call.response.status (HttpStatusCode.Conflict)
-                } else {
-                  app.sexualOrientation.add(newSexualOrientation)
-                  io.ktor.application.call.response.status(HttpStatusCode.Created)
-                  io.ktor.application.call.respond(newSexualOrientation)
-                }
+          if (alreadyExists) {
+            call.response.status (HttpStatusCode.Conflict)
+          } else {
+            app.sexualOrientation.add(newSexualOrientation)
+            call.response.status(HttpStatusCode.Created)
+            call.respond(newSexualOrientation)
+          }
 
-                }
-              }
+          }
 
-              get("/sexual_orientation") {
-                call.response.status (HttpStatusCode.OK)
-                call.respond (app.sexualOrientation)
-              }
-
-              get ("/sexual_orientation/{id}") {
-                val responseSexualOrientation: SexualOrientation? = app.sexualOrientation.firstOrNull {it.id == call.parameters ["id"]}
-
-                if (responseSexualOrientation == null) {
-                  call.response.status (HttpStatusCode.NotFound)
-                } else {
-                  call.response.status (HttpStatusCode.OK)
-                  call.respond (responseSexualOrientation)
-                }
-              }
-
-              put ("/sexual_orientation/{id}") {
-
-                var sexualOrientationToUpdate: SexualOrientation? = app.sexualOrientation.firstOrNull {it.id == call.parameters ["id"]}
-
-                if (sexualOrientationToUpdate == null) {
-                  call.response.status (HttpStatusCode.NotFound)
-                } else {
-
-                    app.sexualOrientation.remove (sexualOrientationToUpdate)
-
-                  var newSexualOrientation: SexualOrientation = call.receive<SexualOrientation>()
-                  newSexualOrientation.id = UUID.nameUUIDFromBytes (newSexualOrientation.label.toLowerCase().toByteArray()).toString()
-
-                  app.sexualOrientation.add (newSexualOrientation)
-                  call.response.status (HttpStatusCode.OK)
-              }
-
-              delete ("/sexual_orientation/{id}") {
-
-                val sexualOrientationToDelete: SexualOrientation? = app.sexualOrientation.firstOrNull {it.id == call.parameters ["id"]}
-
-                if (sexualOrientationToDelete == null) {
-                  call.response.status (HttpStatusCode.NotFound)
-                } else {
-
-                app.sexualOrientation.remove (sexualOrientationToDelete)
-                call.response.status (HttpStatusCode.OK)
-                }
-              }
-
-      //      post ("/interests") {
-      //      get ("/interests") {
-      //      put ("/interests") {
-      //      delete ("/interests") {
-
-            }
+        get("/sexual_orientation") {
+          call.response.status (HttpStatusCode.OK)
+          call.respond (app.sexualOrientation)
         }
+
+        get ("/sexual_orientation/{id}") {
+          val responseSexualOrientation: SexualOrientation? = app.sexualOrientation.firstOrNull {it.id == call.parameters ["id"]}
+
+          if (responseSexualOrientation == null) {
+            call.response.status (HttpStatusCode.NotFound)
+          } else {
+            call.response.status (HttpStatusCode.OK)
+            call.respond (responseSexualOrientation)
+          }
+        }
+
+        put ("/sexual_orientation/{id}") {
+
+          var sexualOrientationToUpdate: SexualOrientation? = app.sexualOrientation.firstOrNull {it.id == call.parameters ["id"]}
+
+          if (sexualOrientationToUpdate == null) {
+            call.response.status (HttpStatusCode.NotFound)
+          } else {
+              app.sexualOrientation.remove(sexualOrientationToUpdate)
+
+              var newSexualOrientation: SexualOrientation = call.receive<SexualOrientation>()
+              newSexualOrientation.id = UUID.nameUUIDFromBytes(newSexualOrientation.label.toLowerCase().toByteArray()).toString()
+
+              app.sexualOrientation.add(newSexualOrientation)
+              call.response.status(HttpStatusCode.OK)
+          }
+        }
+
+        delete ("/sexual_orientation/{id}") {
+
+          val sexualOrientationToDelete: SexualOrientation? = app.sexualOrientation.firstOrNull {it.id == call.parameters ["id"]}
+
+          if (sexualOrientationToDelete == null) {
+            call.response.status (HttpStatusCode.NotFound)
+          } else {
+
+          app.sexualOrientation.remove (sexualOrientationToDelete)
+          call.response.status (HttpStatusCode.OK)
+          }
+        }
+
+          //      post ("/interests") {
+          //      get ("/interests") {
+          //      put ("/interests") {
+          //      delete ("/interests") {
+
+      }
     }.start(wait = true)
 }
