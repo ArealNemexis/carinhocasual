16d86d6d9482bb6c3934df2ed84fab23ea260c2a
Lucas Ferreira <lucas0409lf@gmail.com>
2020-09-29 23:48:11 -0300

    service interface created

---
+++ src/main/kotlin/com/carinhocasual/service/Service.kt
@@ -0,0 +1,19 @@
+package com.carinhocasual.service
+
+
+interface Service {
+//    fun validate ()
+//
+//    fun exist ()
+//
+//    fun getOne ()
+//
+//    fun getAll ()
+//
+//    fun persist ()
+//
+//    fun remove ()
+//
+//    fun replace ()
+
+}
